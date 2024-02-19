698ea560402c49f5a5e96c284e175628f4cb86f8
Marty Wallace <marty@martywallace.com>
2024-02-19 20:08:29 +1100

    add: distance and angle calculations

--- libs/geometry/src/point.spec.ts
+++ libs/geometry/src/point.spec.ts
@@ -5,7 +5,35 @@ describe('Point', () => {
     test('It will calculate the length', () => {
       const point = new Point(5, 8);
 
-      expect(point.length()).toBeCloseTo(9.43398, 5);
+      expect(point.length).toBeCloseTo(9.43398, 5);
+    });
+  });
+
+  describe('angle', () => {
+    test('It will calculate the angle', () => {
+      const point = new Point(12, -6.5);
+
+      expect(point.angle).toBeCloseTo(-0.49642, 5);
+    });
+  });
+
+  describe('distanceTo', () => {
+    test('It will calculate the distance to another point', () => {
+      const point = new Point(0, 0);
+
+      expect(point.distanceTo({ x: 10, y: 0 })).toBe(10);
+      expect(point.distanceTo({ x: -5, y: 0 })).toBe(5);
+    });
+  });
+
+  describe('angleTo', () => {
+    test('It will calculate the angle to another point', () => {
+      const point = new Point(0, 0);
+
+      expect(point.angleTo({ x: 10, y: 0 })).toBe(0);
+      expect(point.angleTo({ x: 0, y: 10 })).toBe(Math.PI / 2);
+      expect(point.angleTo({ x: -10, y: 0 })).toBe(Math.PI);
+      expect(point.angleTo({ x: 0, y: -10 })).toBe(-Math.PI / 2);
     });
   });
 });
--- libs/geometry/src/point.ts
+++ libs/geometry/src/point.ts
@@ -1,22 +1,53 @@
+export interface PointLike {
+  /**
+   * The horizontal value of the point.
+   */
+  x: number;
+
+  /**
+   * The vertical value of the point.
+   */
+  y: number;
+}
+
 /**
  * Defines a point in 2D space.
  */
-export class Point {
+export class Point implements PointLike {
   constructor(
-    /**
-     * The horizontal value of the point.
-     */
     public x = 0,
-    /**
-     * The vertical value of the point.
-     */
     public y = 0,
   ) {}
 
   /**
+   * Calculate the distance from this point to a target point.
+   *
+   * @param target The target point.
+   */
+  public distanceTo(target: PointLike): number {
+    return Math.sqrt((this.x - target.x) ** 2 + (this.y - target.y) ** 2);
+  }
+
+  /**
+   * Calculate the angle from this point to a target point.
+   *
+   * @param target The target point.
+   */
+  public angleTo(target: PointLike): number {
+    return Math.atan2(target.y - this.y, target.x - this.x);
+  }
+
+  /**
    * Calculates the length of this point measured from 0,0.
    */
-  public length(): number {
-    return Math.sqrt(this.x * this.x + this.y * this.y);
+  public get length(): number {
+    return Math.sqrt(this.x ** 2 + this.y ** 2);
+  }
+
+  /**
+   * Calculates the angle of this point measured from 0,0.
+   */
+  public get angle(): number {
+    return Math.atan2(this.y, this.x);
   }
 }
